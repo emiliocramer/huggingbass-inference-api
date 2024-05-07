97e8168de9e0cf8c036a3626f176f243c6bf853d
emiliocramer <emilio.cramer101@gmail.com>
2024-05-07 16:12:42 -0400

    expose comparison routes

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from routes.extraction import extraction_blueprint
 from routes.inference import inference_blueprint
+from routes.comparison import comparison_blueprint
 
 app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(extraction_blueprint)
 app.register_blueprint(inference_blueprint)
+app.register_blueprint(comparison_blueprint)
 
 if __name__ == '__main__':
     app.run()
