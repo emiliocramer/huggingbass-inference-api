8f5c76a4b2d148ed4b259e02aa69d1d15208b48f
emiliocramer <emilio.cramer101@gmail.com>
2024-05-07 19:35:52 -0400

    add cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.extraction import extraction_blueprint
 from routes.inference import inference_blueprint
 from routes.comparison import comparison_blueprint
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow requests from any origin
 
 # Register blueprints
 app.register_blueprint(extraction_blueprint)
