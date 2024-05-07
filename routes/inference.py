c4bc72c30bbda46ff3278ac5ed26ee249d48ea2c
emiliocramer <emilio.cramer101@gmail.com>
2024-05-07 13:22:37 -0400

    change

--- routes/inference.py
+++ routes/inference.py
@@ -88,6 +88,7 @@ def unzip_model_files(zipped_file):
         zip_ref.extractall(tmp_dir)
         extracted_files = os.listdir(tmp_dir)
 
+        print("extracted files: ", extracted_files)
         extracted_folder = os.path.join(tmp_dir, extracted_files[0])
         for file_name in os.listdir(extracted_folder):
             file_path = os.path.join(extracted_folder, file_name)
