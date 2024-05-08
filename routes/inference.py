8b7787d0de4945c3486af1bb187034fc9d712382
emiliocramer <emilio.cramer101@gmail.com>
2024-05-08 16:36:50 -0400

    PUSH TO TEST MODEL WITH MP3

--- routes/inference.py
+++ routes/inference.py
@@ -80,6 +80,8 @@ def process_inferred_audio(model_id, reference_url):
 
     pth_file_url = None
     index_file_url = None
+    tmp_dir = tempfile.mkdtemp()
+
     # Unzip the model file at index 1
     if len(model['fileUrls']) == 0:
         zipped_file_url = model['fileUrls'][0]
@@ -88,6 +90,7 @@ def process_inferred_audio(model_id, reference_url):
         zipped_file_bytes = requests.get(zipped_file_url).content
         zipped_file = BytesIO(zipped_file_bytes)
         pth_file_url, index_file_url = unzip_model_files(zipped_file)
+
     else:
         for file_name in model['fileUrls']:
             file_path = os.path.join(tmp_dir, file_name)
