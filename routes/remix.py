0e130626b011ba91a81591dd4407516f22b7157a
emiliocramer <emilio.cramer101@gmail.com>
2024-05-23 11:22:50 -0400

    changes

--- routes/remix.py
+++ routes/remix.py
@@ -78,7 +78,6 @@ def remix_audio(model_id, reference_url):
     # Clear variables
     pth_file_url = None
     index_file_url = None
-    inferred_audio_url = None
 
     return jsonify({'inferredAudioUrl': inferred_audio_url}), 200
 
